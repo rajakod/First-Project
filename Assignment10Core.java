3c04b6c63f45b77114a205e662609a7b64717f9e
rajakod <44143200+rajakod@users.noreply.github.com>
2018-10-25 19:16:31 -0500

    Add files via upload

---
+++ Assignment10Core.java
@@ -0,0 +1,13 @@
+
+public class Assignment10Core {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for (int i=4; i<10; i++) {
+				if (i%2==0) 
+				System.out.print(i +", ");			
+			}	
+	}
+}
+
+
---
+++ Assignment12Core.java
@@ -0,0 +1,24 @@
+
+public class Assignment12Core {
+
+		public static void main(String[] args) {
+
+			for(int i=1; i<=10; i++) {
+				System.out.println("Hello World ");
+			}
+			
+			int j=1;
+			while(j<=5){
+				System.out.println(" I reached value 5");
+				j++;
+			}
+			
+			int z = 1;
+			do{
+				System.out.println("value of z is "+z);
+				z++;
+			}while(z<=10);
+			
+		}
+
+	}
