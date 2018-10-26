a3c1d34064660468d906a0a2918d4e4ec1ec4e8c
rajakod <44143200+rajakod@users.noreply.github.com>
2018-10-25 19:17:16 -0500

    Add files via upload

---
+++ Assignment11Core.java
@@ -0,0 +1,13 @@
+
+		import java.util.Arrays;
+		import java.util.List;
+
+		public class Assignment11Core {
+
+			public static void main(String[] args) {
+				List<String> list = Arrays.asList("4","5","6", "7","8","9","10","11","12","13");
+				// even print method logic systems.out.println 
+				System.out.print(list.get(4));
+				
+			}
+		}	
